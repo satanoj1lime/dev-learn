846a8407c5688a4dd24530e44aaac6705ff30827
Jonatas Aguiar <crashroot17@gmail.com>
2024-09-15 23:59:02 -0300

    waybar config files updated

--- hyprland-config-git/waybar/start.sh
+++ hyprland-config-git/waybar/start.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-killall waybar
-pkill waybar
+killall -SIGUSR2 waybar
+
 sleep 0.1
-waybar -c ~/Projects/hyprland-config-git/waybar/config.jsonc -s ~/Projects/hyprland-config-git/waybar/style.css &
+waybar
