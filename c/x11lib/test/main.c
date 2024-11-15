0bfc116b04989269d45fbad034509105d0736092
Jonatas Aguiar <crashroot17@gmail.com>
2024-11-14 21:58:18 -0300

    window created

--- c/x11lib/test/main.c
+++ c/x11lib/test/main.c
@@ -1,3 +1,29 @@
 #include <stdio.h>
+
 #include <X11/X.h>
 #include <X11/Xlib.h>
+
+Window win;
+
+int main(void){
+    Display* dsp = XOpenDisplay(NULL); 
+    
+    win = XCreateSimpleWindow(dsp, DefaultRootWindow(dsp), 0, 0,
+                  1280, 720, 0, 0, 0); 
+    
+    XMapWindow(dsp, win);
+
+    XFlush(dsp);
+
+    XEvent ev;
+    while(1){
+        XNextEvent(dsp, &ev);
+    }
+
+    XDestroyWindow(dsp, win);
+    XCloseDisplay(dsp);
+
+    printf("baiano em cima do muro\n");
+
+    return 0;
+}
